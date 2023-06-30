63d497b9d826cf3ec08c28a4f4b8590d83eea3ce
chernandez <chernandez@cloudera.com>
2023-06-30 13:44:20 -0400

    initial commit to create iceberg table from csv

---
+++ code/1b_data_ingest_iceberg.py
@@ -0,0 +1,110 @@
+# ###########################################################################
+#
+#  CLOUDERA APPLIED MACHINE LEARNING PROTOTYPE (AMP)
+#  (C) Cloudera, Inc. 2021
+#  All rights reserved.
+#
+#  Applicable Open Source License: Apache 2.0
+#
+#  NOTE: Cloudera open source products are modular software products
+#  made up of hundreds of individual components, each of which was
+#  individually copyrighted.  Each Cloudera open source product is a
+#  collective work under U.S. Copyright Law. Your license to use the
+#  collective work is as provided in your written agreement with
+#  Cloudera.  Used apart from the collective work, this file is
+#  licensed for your use pursuant to the open source license
+#  identified above.
+#
+#  This code is provided to you pursuant a written agreement with
+#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
+#  this code. If you do not have a written agreement with Cloudera nor
+#  with an authorized and properly licensed third party, you do not
+#  have any rights to access nor to use this code.
+#
+#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
+#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
+#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
+#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
+#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
+#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
+#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
+#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
+#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+#  DATA.
+#
+# ###########################################################################
+
+import os
+import subprocess
+
+from cmlbootstrap import CMLBootstrap
+from pyspark.sql import SparkSession
+from pyspark.sql.utils import AnalysisException
+from pyspark.sql.types import *
+import importlib
+
+data_ingestion = importlib.import_module("1_data_ingestion", package=None)
+
+# Set the setup variables needed by CMLBootstrap
+HOST = os.getenv("CDSW_API_URL").split(":")[0] + "://" + os.getenv("CDSW_DOMAIN")
+USERNAME = os.getenv("CDSW_PROJECT_URL").split("/")[6]  # args.username  # "vdibia"
+API_KEY = os.getenv("CDSW_API_KEY")
+PROJECT_NAME = os.getenv("CDSW_PROJECT")
+
+# Instantiate API Wrapper
+cml = CMLBootstrap(HOST, USERNAME, API_KEY, PROJECT_NAME)
+
+spark = (
+    SparkSession
+        .builder
+        .appName("PythonSQL")
+        .config("spark.sql.extensions","org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+        .config("spark.sql.catalog.spark_catalog","org.apache.iceberg.spark.SparkSessionCatalog")
+        .config("spark.sql.catalog.local","org.apache.iceberg.spark.SparkCatalog")
+        .master("local[*]")
+        .getOrCreate()
+)
+
+# Now we can read in the data into Spark
+storage = os.environ["STORAGE"]
+data_location = os.environ["DATA_LOCATION"]
+hive_database = os.environ["HIVE_DATABASE"]
+hive_table = os.environ["HIVE_TABLE"]
+iceberg_table = f"{hive_table}_iceberg"
+iceberg_table_fq = f"{hive_database}.{hive_table}_iceberg"
+
+if os.environ["STORAGE_MODE"] == "external":
+    path = f"{storage}/{data_location}/WA_Fn-UseC_-Telco-Customer-Churn-.csv"
+else:
+    path = "/home/cdsw/raw/WA_Fn-UseC_-Telco-Customer-Churn-.csv"
+
+if os.environ["STORAGE_MODE"] == "external":
+    telco_data = spark.read.csv(path, header=True, schema=data_ingestion.schema, sep=",", nullValue="NA")
+
+    # This is here to create the Iceberg table if it does not already exist.
+    if iceberg_table not in list(
+        spark.sql("show tables in " + hive_database).toPandas()["tableName"]
+    ):
+        print("creating the " + iceberg_table + " table")
+
+        try:
+            telco_data.write.format("iceberg").mode("overwrite").saveAsTable(
+                iceberg_table_fq
+            )
+        except AnalysisException as ae:
+            print(ae)
+            print("Removing the conflicting directory from storage location.")
+
+            conflict_location = f'{storage}/datalake/data/warehouse/tablespace/external/hive/{iceberg_table}'
+            cmd = ["hdfs", "dfs", "-rm", "-r", conflict_location]
+            subprocess.call(cmd)
+            telco_data.write.format("iceberg").mode("overwrite").saveAsTable(
+                iceberg_table_fq
+            )
+
+        # To get more detailed information about the hive table you can run this:
+        spark.sql("describe formatted " + iceberg_table_fq).toPandas()
